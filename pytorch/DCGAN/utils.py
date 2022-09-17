4f744b1d128dd048c928673582fb234df351f1d6
erfanakk <akbarnezhad1380@gmail.com>
2022-09-17 20:28:31 +0430

    finish DCGANs

--- pytorch/DCGAN/utils.py
+++ pytorch/DCGAN/utils.py
@@ -8,10 +8,10 @@ def save_checkpoint(stete , filname='DCMODEL.pth.tar'):
     print(f'save the model in {filname}')
     torch.save(stete, f=filname)
 
-def load_checkpoint( checkpoint , modelGEN: torch.nn.modules,modelDIS: torch.nn.modules ,  optimizer: torch.optim.Optimizer):
+def load_checkpoint( checkpoint , modelGEN: torch.nn.modules,modelDIS: torch.nn.modules ):
     print('-- loading model --')
     
     modelGEN.load_state_dict(checkpoint['state_dic_GEN'])
     modelDIS.load_state_dict(checkpoint['state_dic_DIS'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
+    
 
